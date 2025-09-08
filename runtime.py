821a92e9a43a34ea62f3e34a1cdcebda9efb6aae
Maya Zakharova <mayzakharova@gmail.com>
2025-09-08 09:51:36 -0500

    fix task

--- runtime.py
+++ runtime.py
@@ -1,13 +1,16 @@
 def get_3rd_value(my_list):
-    return my_list[2]
+    if len(my_list) > 2:
+        return my_list[2]
+    return None
 
 def test_get_3rd_value():
     assert get_3rd_value([0, 1, 2]) == 2
     assert get_3rd_value([0, 1]) == None
 
 def get_last_value(my_list):
-    item_count = len(my_list)
-    return my_list[itm_count]
+    if len(my_list) == 0:
+        return None
+    return my_list[len(my_list) - 1]
 
 def test_get_last_value():
     assert get_last_value([0, 1, 2]) == 2
